f2be35da8c708b1666def163612502992fba7292
chengronggang <123020990@qq.com>
2017-02-08 10:32:03 +0800

    添加enum内容

--- code_language/Swift/Enum.playground/Contents.swift
+++ code_language/Swift/Enum.playground/Contents.swift
@@ -2,4 +2,33 @@
 
 import UIKit
 
+enum SomeEnumeration {
+    //
+}
+
+enum CompassPoint {
+    case North
+    case South
+    case East
+    case West
+}
+
+enum Planet {
+    case Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
+}
+
+var directionToHead = CompassPoint.West
+
+directionToHead = .East
+
+switch directionToHead {
+case .North:
+    print("North")
+case .South:
+    print("South")
+case .West:
+    print("West")
+case .East:
+    print("East")
+}
 
