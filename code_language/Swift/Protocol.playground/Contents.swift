f1b443dc9a52a765852762bb3cc5c09f8f00947e
chengronggang <123020990@qq.com>
2017-02-09 09:26:45 +0800

    添加复习内容

---
+++ code_language/Swift/Protocol.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*
+ 协议语法（Protocol Syntax）
+ 属性要求（Property Requirements）
+ 方法要求（Method Requirements）
+ Mutating 方法要求（Mutating Method Requirements）
+ 构造器要求（Initializer Requirements）
+ 协议作为类型（Protocols as Types）
+ 委托（代理）模式（Delegation）
+ 通过扩展添加协议一致性（Adding Protocol Conformance with an Extension）
+ 通过扩展采纳协议（Declaring Protocol Adoption with an Extension）
+ 协议类型的集合（Collections of Protocol Types）
+ 协议的继承（Protocol Inheritance）
+ 类类型专属协议（Class-Only Protocol）
+ 协议合成（Protocol Composition）
+ 检查协议一致性（Checking for Protocol Conformance）
+ 可选的协议要求（Optional Protocol Requirements）
+ 协议扩展（Protocol Extensions）
+ */
