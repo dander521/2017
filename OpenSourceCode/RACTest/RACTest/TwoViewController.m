bff6def1828f9305d6df5d91c94e8c35f9ae5010
chengronggang <123020990@qq.com>
2017-02-15 10:49:09 +0800

    review ReactiveCocoa

---
+++ OpenSourceCode/RACTest/RACTest/TwoViewController.m
@@ -0,0 +1,50 @@
+//
+//  TwoViewController.m
+//  RACTest
+//
+//  Created by apple on 2017/2/15.
+//  Copyright © 2017年 yuantuan. All rights reserved.
+//
+
+#import "TwoViewController.h"
+
+@interface TwoViewController ()
+
+@end
+
+@implementation TwoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)touchTwoButton:(id)sender
+{
+    // 通知第一个控制器，告诉它，按钮被点了
+    
+    // 通知代理
+    // 判断代理信号是否有值
+    if (self.delegateSignal) {
+        // 有值，才需要通知
+        [self.delegateSignal sendNext:nil];
+    }
+    
+    [self dismissViewControllerAnimated:true completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
